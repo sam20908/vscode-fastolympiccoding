f2258bdeeecd9371baf37e802f6d22a643e2b694
Sam Huang <samestimable2016@gmail.com>
2024-05-31 15:04:04 -0700

    Simplify conditional rendering

--- src/webview/testcases/components/Testcase.tsx
+++ src/webview/testcases/components/Testcase.tsx
@@ -55,22 +55,21 @@ export default function App({
                     <span class="text-base" style={{ whiteSpace: "pre-line" }}>{input}</span>
                 </div>
             </div>
-            {status !== 'RUNNING' ? null :
+            {status === 'RUNNING' &&
                 <div class="flex flex-row">
                     <div class="w-6"></div>
                     <AutoresizeTextarea input={currentInput} onSetInput={setCurrentInput} onKeyUp={handleKeyUp} />
                 </div>
             }
-            {
-                stderr === "" ? null :
-                    <div class="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-2 mt-1">
-                            <path fill={"#6C4549"} fillRule="evenodd" d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z" clipRule="evenodd" />
-                        </svg>
-                        <div class="grow">
-                            <span class="text-base" style={{ whiteSpace: "pre-line" }}>{stderr}</span>
-                        </div>
+            {stderr !== "" &&
+                <div class="flex flex-row">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-2 mt-1">
+                        <path fill={"#6C4549"} fillRule="evenodd" d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z" clipRule="evenodd" />
+                    </svg>
+                    <div class="grow">
+                        <span class="text-base" style={{ whiteSpace: "pre-line" }}>{stderr}</span>
                     </div>
+                </div>
             }
             <div class="flex flex-row">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-2 mt-1">
