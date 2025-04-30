8229429a8035a907303f4e90dadffc39a76d3dcd
sam20908 <samestimable2016@gmail.com>
2025-04-30 15:36:41 -0700

    Now I don't need to disable these eslint lines???

--- src/views/judge/webview/message.ts
+++ src/views/judge/webview/message.ts
@@ -1,7 +1,7 @@
 import { ProviderMessage } from '../message';
 
-const vscode = acquireVsCodeApi(); // eslint-disable-line
+const vscode = acquireVsCodeApi();
 
 export const postProviderMessage = (msg: ProviderMessage) => {
-  vscode.postMessage(msg); // eslint-disable-line
+  vscode.postMessage(msg);
 };
