fcb70cd16725df75d938deb6bd264a5698b6f0f3
sam20908 <samestimable2016@gmail.com>
2025-05-03 00:19:54 -0700

    test

--- src/utils/vscode.ts
+++ src/utils/vscode.ts
@@ -195,6 +195,7 @@ export function resolveVariables(
 		'${pathSeparator}': path.sep,
 		'${/}': path.sep,
 		'${exeExtname}': os.platform() === 'win32' ? '.exe' : '',
+		asdf,
 	};
 
 	// Replace all regexes with their matches at once
